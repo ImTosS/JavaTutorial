299b81da9ccb38940610786ef9f576758e4fc70a
Oscar Urbina <im_toss@hotmail.com>
2020-10-02 20:00:21 -0500

    Adding Athlete and Customer objects.

---
+++ src/main/java/models/Athlete.java
@@ -0,0 +1,13 @@
+package models;
+
+public class Athlete extends Person{
+
+    public Athlete(String name, String lastName, String maidenName, int height, int weight, int depth){
+        super(name, lastName, maidenName, height, weight, depth);
+    }
+
+    @Override
+    public void salute(){
+        System.out.println("Soy ATLETA y mi nombre es: " + super.name + " " + super.lastName);
+    }
+}
---
+++ src/main/java/models/Customer.java
@@ -0,0 +1,33 @@
+package models;
+
+/**
+ * The type Customer.
+ */
+public class Customer extends Person{
+
+    /**
+     * Instantiates a new Customer.
+     */
+    public Customer(){
+        super();
+    }
+
+    /**
+     * Instantiates a new Customer.
+     *
+     * @param name       the name
+     * @param lastName   the last name
+     * @param maidenName the maiden name
+     * @param height     the height
+     * @param weight     the weight
+     * @param depth      the depth
+     */
+    public Customer(String name, String lastName, String maidenName, int height, int weight, int depth){
+        super(name, lastName, maidenName, height, weight, depth);
+    }
+
+    @Override
+    public void salute(){
+        System.out.println("Soy CUSTOMER y mi nombre es: " + super.name + " " + super.lastName);
+    }
+}
