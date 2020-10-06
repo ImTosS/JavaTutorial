b53df283f8a2ac3d34dbc821070ffdb1ca62d623
Oscar Urbina <im_toss@hotmail.com>
2020-10-06 17:28:08 -0500

    Adding credentials object.

---
+++ src/main/java/models/Credentials.java
@@ -0,0 +1,39 @@
+package models;
+
+/**
+ * The type Credentials.
+ */
+public class Credentials {
+
+    private String username;
+    private String password;
+
+    /**
+     * Instantiates a new Credentials.
+     *
+     * @param username the username
+     * @param password the password
+     */
+    public Credentials(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    /**
+     * Gets username.
+     *
+     * @return the username
+     */
+    public String getUsername() {
+        return username;
+    }
+
+    /**
+     * Gets password.
+     *
+     * @return the password
+     */
+    public String getPassword() {
+        return password;
+    }
+}
