993f801b5b95b842399d4e55343ec5488723d1fc
Oscar Urbina <im_toss@hotmail.com>
2020-10-06 17:28:39 -0500

    Adding setCredentials behavior. Overwitting behaviors.

--- src/main/java/models/PaypalGateway.java
+++ src/main/java/models/PaypalGateway.java
@@ -1,9 +1,78 @@
 package models;
 
+/**
+ * The type Paypal gateway.
+ */
 public class PaypalGateway extends PaymentGateway{
 
     @Override
     public void executeCharge(){
         System.out.println("Se ha realizado el cargo con Paypal!");
+    }
+
+    /**
+     * Sets credentials.
+     *
+     * @param credentials the credentials
+     */
+    public void setCredentials(Credentials credentials)
+    {
+        this.authenticate(credentials.getUsername(), credentials.getPassword());
+    }
+
+    /**
+     * Sets credentials.
+     *
+     * @param username the username
+     * @param password the password
+     */
+    public void setCredentials(String username, String password)
+    {
+        this.authenticate(username, password);
+    }
+
+    /**
+     * Sets credentials.
+     *
+     * @param username    the username
+     * @param password    the password
+     * @param environment the environment
+     */
+    public void setCredentials(String username, String password, String environment)
+    {
+        System.out.println("Implementacion con 3 parametros");
+    }
+
+    /**
+     * Sets credentials.
+     *
+     * @param username    the username
+     * @param password    the password
+     * @param environment the environment
+     * @param devEmail    the dev email
+     */
+    public void setCredentials(String username, String password, String environment, String devEmail)
+    {
+        System.out.println("Implementacion con 4 parametros, que es dev email");
+    }
+
+    /**
+     * Sets credentials.
+     *
+     * @param username    the username
+     * @param password    the password
+     * @param environment the environment
+     * @param connections the connections
+     */
+    public void setCredentials(String username, String password, String environment, int connections)
+    {
+        System.out.println("Implementacion con 4 parametros, con max connections");
+    }
+
+    private void authenticate(String username, String password){
+        System.out.println("::::Authenticating user::::");
+        System.out.println("Username:" + username);
+        System.out.println("Password:" + password);
+        System.out.println("::::USER AUTHENTICATED::::");
     }
 }
