a04aadc6537d3d8aeeaedd6218e98fbbfa20bfc6
Oscar Urbina <im_toss@hotmail.com>
2020-10-09 18:12:44 -0500

    Renaming class.

--- src/main/java/models/ChargeService.java
+++ src/main/java/models/ChargeService.java
@@ -2,7 +2,7 @@ package models;
 
 import interfaces.Chargable;
 
-public class ProcessCharge {
+public class ChargeService {
 
     public void charge(Chargable chargable){
         chargable.executeCharge();
