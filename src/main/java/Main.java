459972777fb8bbbb4b191b2da0272c19f8198f9f
Oscar Urbina <im_toss@hotmail.com>
2020-10-09 18:13:00 -0500

    Improving interfaces example.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,5 @@
+import interfaces.Printable;
 import models.*;
-
-import javax.swing.*;
-import java.security.cert.CRLReason;
-import java.text.CollationKey;
 
 class Main {
 
@@ -11,19 +8,26 @@ class Main {
 //        Person person = new Customer("Antonio","Aguirre","Rivera", 172, 70, 35);
 //        person.giveMeYourFullName();
 
-
 //        Person person = new Customer("Antonio","Aguirre","Rivera", 172, 70, 35);
 
 //        Athlete athlete = new Athlete();
 
 //        House house = new House();
 //        house.welcome(person);
-        PaypalGateway paymentGateway = new PaypalGateway();
 
-        Credentials credentials = new Credentials("oscar","secure-password");
-        paymentGateway.setCredentials(credentials);
 
-        ProcessCharge processCharge = new ProcessCharge();
-        processCharge.charge(paymentGateway);
+        StripeGateway stripeGateway = new StripeGateway();
+
+//        Credentials credentials = new Credentials("oscar","secure-password");
+//        paymentGateway.setCredentials(credentials);
+
+        ChargeService chargeService = new ChargeService();
+        chargeService.charge(stripeGateway);
+
+        NotificationService notificationService = new NotificationService();
+        notificationService.notificate(stripeGateway);
+
+        PrintService printService = new PrintService();
+        printService.printSomething(stripeGateway);
     }
 }
