9bc0573acc0d8b3f36c0663531c47f9c9fb99867
Oscar Urbina <im_toss@hotmail.com>
2020-10-09 18:11:48 -0500

    Adding example interfaces

---
+++ src/main/java/interfaces/Chargable.java
@@ -0,0 +1,12 @@
+package interfaces;
+
+/**
+ * The interface Chargable.
+ */
+public interface Chargable {
+
+    /**
+     * Execute charge.
+     */
+    void executeCharge();
+}
---
+++ src/main/java/interfaces/Notificable.java
@@ -0,0 +1,12 @@
+package interfaces;
+
+/**
+ * The interface Notificable.
+ */
+public interface Notificable {
+
+    /**
+     * Send notification.
+     */
+    void sendNotification();
+}
