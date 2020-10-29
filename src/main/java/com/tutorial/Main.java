8b2e956303c3255a658ecf7f25561dfad913376e
Antonio Aguirre <antonio_aar@hotmail.es>
2020-10-29 02:09:19 -0600

    Some code commented and solved ToDo activities and we added the code below of ToDo comment

--- src/main/java/com/tutorial/Main.java
+++ src/main/java/com/tutorial/Main.java
@@ -1,13 +1,6 @@
 package com.tutorial;
 
-import com.tutorial.application.charge.ChargeService;
-import com.tutorial.domain.exceptions.InvalidCredentialsException;
-import com.tutorial.domain.models.Credentials;
-import com.tutorial.infrastructure.gateways.*;
-import com.tutorial.infrastructure.notifications.EmailNotification;
-import com.tutorial.infrastructure.notifications.Notificable;
-import com.tutorial.infrastructure.printing.LocalPrinter;
-import com.tutorial.infrastructure.printing.Printable;
+import com.tutorial.infrastructure.ExampleofAbstract_Interface_Inheritance.*;
 
 class Main {
 
@@ -23,7 +16,7 @@ class Main {
 //        House house = new House();
 //        house.welcome(person);
 
-
+        /*
         PaymentGateway paymentGateway = new OpenPayGateway();
 
         Credentials credentials = new Credentials(" ", " ");
@@ -36,14 +29,20 @@ class Main {
 
         ChargeService chargeService = new ChargeService();
         chargeService.charge(paymentGateway);
+        */
 
+        // Testing..
+        //Person person = new Athlete("Antonio","Aguirre","Rivera", 172, 70, 35);
+        //person.salute();
 
         //TODO::ADD EXAMPLE FOR ABSTRACT CLASSES
-
         //TODO::ADD EXAMPLE FOR INTERFACE CLASSES
-
         //TODO::ADD EXAMPLE FOR INHERITANCE CLASSES
 
+        // We have two Quadrilateral figures: rectangle and square.
+        // In case of rectangle figure, asume sideA and sideB are a pair of equals sides
+        QuadrilateralFigure square = new Square(4,4,4,4);
+        square.infoFigure();
 
     }
 }
