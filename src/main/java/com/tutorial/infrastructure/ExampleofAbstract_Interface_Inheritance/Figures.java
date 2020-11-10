c1332af3f98e61a8076db3358011f1c1e59f3f8d
Antonio Aguirre <antonio_aar@hotmail.es>
2020-11-10 17:38:23 -0600

    Added set and get for the color variable

--- src/main/java/com/tutorial/infrastructure/ExampleofAbstract_Interface_Inheritance/Figures.java
+++ src/main/java/com/tutorial/infrastructure/ExampleofAbstract_Interface_Inheritance/Figures.java
@@ -1,11 +1,11 @@
 package com.tutorial.infrastructure.ExampleofAbstract_Interface_Inheritance;
 
-public abstract class Figures implements Paintable{
+public abstract class Figures implements Printable {
 
     protected String colour;
 
     final public void infoFigure(){
-        this.paintFigure();
+        this.print();
         System.out.println("El perímetro es: " + this.calculatePerimeter() + " unidades.");
         System.out.println("El área es: " +this.calculateArea() + " unidades cuadradas.");
     }
@@ -13,4 +13,12 @@ public abstract class Figures implements Paintable{
     abstract protected double calculateArea();
 
     abstract protected double calculatePerimeter();
+
+    public void setColour(String colour) {
+        this.colour = colour;
+    }
+
+    protected String getColour() {
+        return colour;
+    }
 }
